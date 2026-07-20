733730990a904fb382837bf26dd26c045b854e55
Guillain BISIMWA <guillainbisimwa@hotmail.fr>
2026-07-20 20:44:18 +0200

    Add src/modules/screenings/screenings.module.ts.

---
+++ src/modules/screenings/screenings.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Screening, ScreeningSchema } from './schemas/screening.schema';
+import { ScreeningsService } from './screenings.service';
+import { ScreeningsController } from './screenings.controller';
+import { AnchorService } from './anchor.service';
+import { BlockchainModule } from '../blockchain/blockchain.module';
+import { ClinicsModule } from '../clinics/clinics.module';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Screening.name, schema: ScreeningSchema }]),
+    BlockchainModule,
+    ClinicsModule,
+  ],
+  providers: [ScreeningsService, AnchorService],
+  controllers: [ScreeningsController],
+  exports: [ScreeningsService],
+})
+export class ScreeningsModule {}
