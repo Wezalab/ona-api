1bf4caf34d76cac47339da9727f517bd664ea0f9
Guillain BISIMWA <guillainbisimwa@hotmail.fr>
2026-07-20 19:50:49 +0200

    Add src/common/decorators/roles.decorator.ts.

---
+++ src/common/decorators/roles.decorator.ts
@@ -0,0 +1,7 @@
+import { SetMetadata } from '@nestjs/common';
+import { Role } from '../enums/role.enum';
+
+export const ROLES_KEY = 'roles';
+
+/** Restrict a route (or controller) to the given roles. Used with RolesGuard. */
+export const Roles = (...roles: Role[]) => SetMetadata(ROLES_KEY, roles);
