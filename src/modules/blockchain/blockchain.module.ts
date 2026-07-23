183db0487efac00f3b466b5baf6389a64b040d51
Guillain BISIMWA <guillainbisimwa@hotmail.fr>
2026-07-23 20:55:45 +0200

    Import ScreeningsModule (forwardRef) to inject AnchorService

--- src/modules/blockchain/blockchain.module.ts
+++ src/modules/blockchain/blockchain.module.ts
@@ -1,8 +1,10 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { StarknetService } from './starknet.service';
 import { BlockchainController } from './blockchain.controller';
+import { ScreeningsModule } from '../screenings/screenings.module';
 
 @Module({
+  imports: [forwardRef(() => ScreeningsModule)],
   providers: [StarknetService],
   controllers: [BlockchainController],
   exports: [StarknetService],
