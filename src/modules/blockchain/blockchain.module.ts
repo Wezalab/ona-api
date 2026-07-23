36e698176141a3d0eb421b928c11f4c2ed11d8f3
Guillain BISIMWA <guillainbisimwa@hotmail.fr>
2026-07-23 21:01:15 +0200

    Fix blockchain.module.ts: remove ScreeningsModule import — avoid circular dep

--- src/modules/blockchain/blockchain.module.ts
+++ src/modules/blockchain/blockchain.module.ts
@@ -1,10 +1,8 @@
-import { forwardRef, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { StarknetService } from './starknet.service';
 import { BlockchainController } from './blockchain.controller';
-import { ScreeningsModule } from '../screenings/screenings.module';
 
 @Module({
-  imports: [forwardRef(() => ScreeningsModule)],
   providers: [StarknetService],
   controllers: [BlockchainController],
   exports: [StarknetService],
